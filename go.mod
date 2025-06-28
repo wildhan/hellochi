ad46e0ce1ccdc4136583c197000760c0238e9716
Wildhan Ibrahim <wildhan.ibrahim@gmail.com>
2025-06-28 12:22:13 +0700

    init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module hellochi
+
+go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.2.2
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)    // Example middleware usage
+	r.Use(middleware.Recoverer) // Example middleware usage
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	r.Get("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		w.Write([]byte("Hello, " + name + "!"))
+	})
+	http.ListenAndServe(":8080", r)
+}
